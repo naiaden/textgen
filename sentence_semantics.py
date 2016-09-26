dc76fb675548f975f59387e56f87bf1a8a84f1e0
Florian Kunneman <f.kunneman@gmail.com>
2016-09-26 13:27:52 +0200

    fixed SentenceSemantics class, added implementation to witc.py

--- sentence_semantics.py
+++ sentence_semantics.py
@@ -28,15 +28,24 @@ class SentenceSemantics:
         return clean_sentences
 
     def sentence_similarity(self,sentence1,sentence2):
-        return self.model.n_similarity([word for word in sentence1 if word in self.model.vocab], [word for word in sentence2 if word in self.model.vocab])
+        sen1_match = [word for word in sentence1 if word in self.model.vocab]
+        sen2_match = [word for word in sentence2 if word in self.model.vocab]
+        if len(sen1_match)>0 and len(sen2_match)>0:
+            sim = self.model.n_similarity(sen1_match, sen2_match)
+        else:
+            sim = 0
+        return sim
+        
+        #return self.model.n_similarity([word for word in sentence1 if word in self.model.vocab], [word for word in sentence2 if word in self.model.vocab])
 
     def rank_sentences_similarity(self,target_sentence,source_sentences):
         sentence_similarity = []
         for source_sentence in source_sentences:
-            index_similarity.append((i,self.sentence_similarity(target_sentence,source_sentence)))
-        sentence_similarity_sorted = sorted(index_similarity,key=lambda k : k[1],reverse=True)
+            sensim = self.sentence_similarity(target_sentence,source_sentence)
+            sentence_similarity.append((source_sentence,sensim))
+        sentence_similarity_sorted = sorted(sentence_similarity,key=lambda k : k[1],reverse=True)
         return sentence_similarity_sorted
 
-    def return_sentence_candidates(self,target_sentence,source_sentences,candidate_ranks=[10,50,100]):
+    def return_sentence_candidates(self,target_sentence,source_sentences,candidate_ranks=[10,50,70]):
         sorted_sentences = self.rank_sentences_similarity(target_sentence,source_sentences)
         return [sorted_sentences[i] for i in candidate_ranks]
--- witc.py
+++ witc.py
@@ -3,15 +3,20 @@
 #
 import configparser
 import json
+import random
+
 from languagemodel import LanguageModel
 from textgenerator import TextGenerator
 from normaliser import FrogNormaliser
+from sentence_semantics import SentenceSemantics
 
 config = configparser.ConfigParser()
 config.read('witc.ini')
 
 cache_file = config['INPUT']['CacheFile']
 minimum_paragraph_length = int(config['OUTPUT'].get('MinimumParagraphLength', 30))
+w2v_model = config['INPUT']['W2V']
+testout = config['OUTPUT']['StoryFile']
 
 if cache_file:
     print("+ Reading cache file")
@@ -24,7 +29,20 @@ fn = FrogNormaliser(data)
 print("+ Creating language model")
 lm = LanguageModel(data)
 
+print("+ Loading Word2Vec model")
+ss = SentenceSemantics()
+ss.load_model(w2v_model)
+
 print("+ Generating text")
 tg = TextGenerator(lm, minimum_paragraph_length)
-print(tg.generate_paragraph())
+with open(testout,'w',encoding='utf-8') as t_out:
+    target = tg.generate_sentence()
+    t_out.write('start : ' + ' '.join(target) + '\n')
+    for i in range(50):
+        sources = [tg.generate_sentence() for x in range(150)]
+        candidates = ss.return_sentence_candidates(target,sources)
+        target = random.choice([x[0] for x in candidates])
+        t_out.write(str(i) + ' : ' + ' '.join(target) + '\n')
+
+#print(tg.generate_paragraph()) 
 
