1de34b56aa21cfaf8c60c05a4906a49963ad3294
naiaden <louis@naiaden.nl>
2017-08-28 21:03:58 +0200

    All basic functions for api implemented

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
@@ -8,17 +8,33 @@ db_connect = create_engine('sqlite:///witc.db')
 app = Flask(__name__)
 api = Api(app)
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 class Sentences(Resource):
     def get(self):
         conn = db_connect.connect()
         query = conn.execute("select * from Sentences")
         return {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
 
+class Sentence(Resource):
+    def get(self, sentence_id):
+        conn = db_connect.connect()
+        query = conn.execute("select * from Sentences where sentence_id =%d " %int(sentence_id))
+        return {'data': [dict(zip(tuple (query.keys()), i)) for i in query.cursor]}
+
 class Stories(Resource):
     def get(self):
         conn = db_connect.connect()
         query = conn.execute("select * from Stories")
         return {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+
+class Story(Resource):
+    def get(self, story_id):
+        conn = db_connect.connect()
+        query = conn.execute("select * from Stories where story_id =%d " %int(story_id))
+        return {'data': [dict(zip(tuple (query.keys()), i)) for i in query.cursor]}
 
 class Authors(Resource):
     def get(self):
@@ -41,6 +57,7 @@ class Vote(Resource):
     def put(self, sentence_id):
         something = request.form['data']
         # if number and number corresponds to sentence
+        conn = db_connect.connect()
         query = conn.execute("select sentence_id from Sentences")
         sentence_ids = [i for i in query.cursor]
         if (int(sentence_id),) in sentence_ids:
@@ -56,8 +73,8 @@ api.add_resource(Stories, '/stories')
 api.add_resource(Authors, '/authors')
 api.add_resource(Votes, '/votes')
 
-#api.add_resource(Sentence, '/sentence/<sentence_id>')
-#api.add_resource(Story, '/story/<story_id>')
+api.add_resource(Sentence, '/sentence/<sentence_id>')
+api.add_resource(Story, '/story/<story_id>')
 api.add_resource(Vote, '/vote/<sentence_id>')
 
 if __name__ == '__main__':
